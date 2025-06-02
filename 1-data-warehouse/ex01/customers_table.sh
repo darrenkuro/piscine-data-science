80a8eb16664c45c673f03e611ebc1cc4f9f2ce8b
darrenkuro <odon5ht@gmail.com>
2025-06-02 18:28:42 +0200

    Add customers_table.sh to ex01 in 1-data-warehouse

---
+++ 1-data-warehouse/ex01/customers_table.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DB_NAME="piscineds"
+DB_USER="dlu"
+DB_HOST="localhost"
+DB_PORT="5432"
+
+export PGPASSWORD="mysecretpassword"
+
+tables=$(psql -U "$DB_USER" -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -Atc "
+SELECT tablename FROM pg_tables
+WHERE tablename ~ '^data_202[0-9]_.{3}$';")
+
+if [ -z "$tables" ]; then
+    echo "No table with matching name found."
+    exit 1
+fi
+
+unionsql=""
+for table in $tables; do
+    if [ -z "$unionsql" ]; then
+        unionsql="SELECT * FROM \"$table\""
+    else
+        unionsql="$unionsql UNION ALL SELECT * FROM \"$table\""
+    fi
+done
+
+psql -U "$DB_USER" -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -c "
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers AS $unionsql;"
