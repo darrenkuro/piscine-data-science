6d3fa1b072c201d8044f7c1de85621046749ef2a
darrenkuro <odon5ht@gmail.com>
2025-06-05 21:16:08 +0200

    Update fusion script to clean up rows with dup product id

--- 1-data-warehouse/ex03/fusion.sh
+++ 1-data-warehouse/ex03/fusion.sh
@@ -13,13 +13,23 @@ ALTER TABLE customers ADD COLUMN IF NOT EXISTS category_id BIGINT;
 ALTER TABLE customers ADD COLUMN IF NOT EXISTS category_code TEXT;
 ALTER TABLE customers ADD COLUMN IF NOT EXISTS brand TEXT;
 
+WITH merged_items AS (
+  SELECT
+    product_id,
+    MAX(category_id) AS category_id,
+    MAX(category_code) AS category_code,
+    MAX(brand) AS brand
+  FROM items
+  GROUP BY product_id
+)
+
 UPDATE customers c
 SET
-    category_id = i.category_id,
-    category_code = i.category_code,
-    brand = i.brand
-FROM items i
-WHERE c.product_id = i.product_id;
+    category_id = mi.category_id,
+    category_code = mi.category_code,
+    brand = mi.brand
+FROM merged_items mi
+WHERE c.product_id = mi.product_id;
 "
 
 psql -U "$DB_USER" -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" -c "$CMD"
