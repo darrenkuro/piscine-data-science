dd1c56dc35e517c7b3a76165f7051d6ab0d93b4d
darrenkuro <odon5ht@gmail.com>
2025-06-07 23:21:22 +0200

    Add plot to variance

--- 4-data-scientist-p2/ex02/variances.py
+++ 4-data-scientist-p2/ex02/variances.py
@@ -2,6 +2,7 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv("../Train_knight.csv")
 X = df.drop(columns=["knight"]) 
@@ -22,3 +23,10 @@ print(cumulative_var)
 # How many components to reach 90%?
 n_components_90 = (cumulative_var < 90).sum() + 1
 print(f"\nComponents needed to reach 90% variance: {n_components_90}")
+
+plt.figure(figsize=(8, 5))
+plt.plot(np.arange(1, len(cumulative_var) + 1), cumulative_var)
+plt.xlabel('Number of components')
+plt.ylabel('Explained Variance (%)')
+plt.tight_layout()
+plt.savefig("variance.png")
