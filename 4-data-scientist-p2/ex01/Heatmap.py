7c16f5ebf20652f67482c9e8e75473d29c809bb5
darrenkuro <odon5ht@gmail.com>
2025-06-08 02:34:07 +0200

    Refactor heatmap

--- 4-data-scientist-p2/ex01/Heatmap.py
+++ 4-data-scientist-p2/ex01/Heatmap.py
@@ -2,20 +2,27 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("../Train_knight.csv")
-df["knight"] = df["knight"].map({"Jedi": 0, "Sith": 1})
-corr_matrix = df.corr(numeric_only=True)
+FILE_TRAIN = "../Train_knight.csv"
 
-plt.figure(figsize=(10, 10))
-sns.heatmap(
-    corr_matrix,
-    cmap="magma",
-    center=0,
-    annot=False,
-    square=True,
-    linewidths=0,
-    cbar_kws={"shrink": 0.8}
-)
+try:
+    df = pd.read_csv(FILE_TRAIN)
+    df["knight"] = df["knight"].map({"Jedi": 0, "Sith": 1})
+    corr_matrix = df.corr(numeric_only=True)
 
-plt.tight_layout()
-plt.savefig("heatmap.png")
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(
+        corr_matrix,
+        cmap="magma",
+        center=0,
+        annot=False,
+        square=True,
+        linewidths=0,
+        cbar_kws={"shrink": 0.8}
+    )
+
+    plt.tight_layout()
+    plt.savefig("heatmap.png")
+    plt.close()
+except Exception as e:
+    print(f"Error: {e}")
+    exit(1)
